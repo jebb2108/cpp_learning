32020b51968e7525d62d0c8fa6d56cb7330ebc1d
jebb <gabouchard2002@gmail.com>
2024-12-01 19:21:41 +0300

    first commit

---
+++ milesInKm/milesInKm.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+    //
+    const double mileInKm=1.6;
+    /* переменные для записи расстояния 
+    в милях и километрах */
+    double distKm, distMile;
+    // Запрос на ввод расстояния в милях
+    cout<<"Введите количество миль: ";
+    // Считывания значения для расстояния в милях
+    cin>>distMile;
+    // Вычисление расстояния в километрах
+    distKm=distMile*mileInKm;
+    // Отображение результата вычислений
+    cout<<"Получившееся расстояние в километрах: "<<distKm<<endl;
+
+    return 0;
+}
