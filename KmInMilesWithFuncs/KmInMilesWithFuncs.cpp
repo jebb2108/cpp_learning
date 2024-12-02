b1ebf9f996d003c0e3eb497db740bd6f1b2a10c2
jebb <gabouchard2002@gmail.com>
2024-12-02 11:21:30 +0300

    learned to make functions in C++

---
+++ KmInMilesWithFuncs/KmInMilesWithFuncs.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+// Функция для считывания расстояния в милях:
+double getDistMile() {
+    // Переменная для записи результата функции:
+    double dist;
+    // Запрос на ввод расстояния в милях:
+    cout<<"Укажите расстояние в милях: ";
+    // Считывание значения для расстояния в милях:
+    cin>>dist;
+    // Результат функции:
+    return dist;
+}
+// Функция для перевода милей в киллометры:
+double getDistKm(double dist) {
+    // В одной миле километров:
+    double distMile=1.609344;
+    // Результат функции:
+    return dist*distMile;
+}
+
+int main() {
+    // Переменная для записи расстояния в милях:
+    double distMile=getDistMile();
+    // Выполнение вычислений:
+    cout<<"Расстояние (в км): "<<getDistKm(distMile)<<endl;
+    return 0;
+}
