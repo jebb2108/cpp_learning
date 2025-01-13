93159a1ade721603fa116192ebd7631dcdd833a8
jebb <gabouchard2002@gmail.com>
2025-01-13 18:38:25 +0300

    few more lessons

--- learning_stuff/chapterFour/recursion.cpp
+++ learning_stuff/chapterFour/recursion.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <ciso646>
 
 // В функции используется рекурсивный вызов:
 double getMoney(double m, double r, int y)
 {
-    if(y==0)
+
+    if(y==0 and double(y)/2==0)
     {
         return m;
     }
@@ -16,17 +18,18 @@ double getMoney(double m, double r, int y)
 // Главная функция программы:
 int main()
 {
+    using namespace std;
     // Начальная сумма вклада:
     double money=1000;
     // Процентная ставка:
     double rate=5;
-    std::cout<<"Начальная сумма: "<<money<<std::endl;
-    std::cout<<"Годовая ставка: "<<rate<<"%\n";
+    cout<<"Начальная сумма: "<<money<<endl;
+    cout<<"Годовая ставка: "<<rate<<"%\n";
     // Вычисление дохода за разные промежутки времени:
-    std::cout<<"Вклад на один год: "<<getMoney(money, rate, 1)<<std::endl;
-    std::cout<<"Вклад на 7 лет:"<<getMoney(money, rate, 7)<<std::endl;
-    std::cout<<"Вклад на 10 лет: ";
-    std::cout<<getMoney(money, rate, 10)<<std::endl;
+    cout<<"Вклад на один год: "<<getMoney(money, rate, 1)<<endl;
+    cout<<"Вклад на 7 лет:"<<getMoney(money, rate, 7)<<endl;
+    cout<<"Вклад на 10 лет: ";
+    cout<<getMoney(money, rate, 10)<<endl;
 
     return 0;
 }
